359442b5a146957d3e1ca3588cfcdad724ce2f40
Matas Mazeika <matas@Matass-MacBook-Air.local>
2021-11-30 18:21:28 -0500

    add gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,83 +1,98 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-	siteMetadata: {
-		siteUrl: 'https://www.yourdomain.tld',
-		title: 'My Gatsby Site',
-	},
-	plugins: [
-		{
-			resolve: `gatsby-plugin-netlify-cms`,
-			options: {
-				manualInit: true,
-				modulePath: `${__dirname}/src/cms/cms.js`,
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-sass',
-			options: {
-				additionalData: `@import "${__dirname}/src/assets/scss/abstracts";`,
-			},
-		},
-		'gatsby-plugin-image',
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-sitemap',
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				name: `GatsbyJS`,
-				short_name: `GatsbyJS`,
-				start_url: `/`,
-				background_color: `#f7f0eb`,
-				theme_color: `#a2466c`,
-				display: `standalone`,
-				icon: 'src/images/icons/logo.svg',
-			},
-		},
-		'gatsby-plugin-mdx',
-		'gatsby-plugin-sharp',
-		'gatsby-transformer-sharp',
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'images',
-				path: './src/images/',
-			},
-			__key: 'images',
-		},
-		{
-			resolve: 'gatsby-plugin-google-fonts-v2',
-			options: {
-				fonts: [
-					{
-						family: 'Montserrat',
-						weights: ['400', '700'],
-					},
-				],
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-root-import',
-			options: {
-				resolveModules: [path.join(__dirname, 'libs')],
-				'@': path.join(__dirname, 'src'),
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-react-svg',
-			options: {
-				rule: {
-					include: /icons/,
-				},
-			},
-		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'pages',
-				path: './src/pages/',
-			},
-			__key: 'pages',
-		},
-	],
-};
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "My Gatsby Site",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        manualInit: true,
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        additionalData: `@import "${__dirname}/src/assets/scss/abstracts";`,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: "src/images/icons/logo.svg",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-plugin-google-fonts-v2",
+      options: {
+        fonts: [
+          {
+            family: "Montserrat",
+            weights: ["400", "700"],
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        resolveModules: [path.join(__dirname, "libs")],
+        "@": path.join(__dirname, "src"),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /icons/,
+        },
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: process.env.GTM_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [process.env.GTAG_ID],
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
+  ],
+}
